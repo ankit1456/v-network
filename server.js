16b7f006859cdfdd6b8700bc6230767cf8b7fb7b
ankittripathi <ankittripathi1456@gmail.com>
2023-03-19 13:08:59 +0530

    initial commit

--- server.js
+++ server.js
@@ -46,12 +46,12 @@ mongoose.connect(
   }
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 
 const port = process.env.PORT || 5000;
 http.listen(port, () => {
